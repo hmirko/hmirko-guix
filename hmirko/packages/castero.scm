281cb34a15cc7c643446ad0569e1509221e4f862
Mirko Hartung <hmirko@posteo.de>
2019-03-03 21:04:11 +0100

    fix package license

--- hmirko/packages/castero.scm
+++ hmirko/packages/castero.scm
@@ -33,7 +33,8 @@
     (home-page "https://xgi.github.io/castero")
     (synopsis "castero is a TUI podcast client for the terminal")
     (description "castero is a TUI podcast client for the terminal. This version supports playback in mpv.")
-    (license license:mit)))
+    (license license:agpl3) ; TODO the source repository claims MIT
+    ))
 
 (define-public python-mpv
   (package
