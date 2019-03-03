caaea296139bc7d1d3f60b8586c845f4f6b2831f
Mirko Hartung <hmirko@posteo.de>
2019-03-03 20:46:32 +0100

    add missing lib

--- hmirko/packages/castero.scm
+++ hmirko/packages/castero.scm
@@ -32,4 +32,29 @@
     (home-page "https://xgi.github.io/castero")
     (synopsis "castero is a TUI podcast client for the terminal")
     (description "castero is a TUI podcast client for the terminal. This version supports playback in mpv.")
-    (license license:gpl3+)))
+    (license license:mit)))
+
+(define-public python-mpv
+  (package
+    (name "python-mpv")
+    (version "0.3.9")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://github.com/jaseg/python-mpv/archive/v"
+			   version
+			   ".tar.gz"))
+       (sha256
+        (base32
+         "109c6wv9c0a77ha7v1nzcx4xagj84yjpa993rb32xx2hvz8mdsii"))))
+    (build-system python-build-system)
+    (inputs
+     `(("mpv" ,mpv)
+       ))
+    (arguments
+     `(#:tests? #f                      ;no tests
+       ))
+    (home-page "https://github.com/jaseg/python-mpv")
+    (synopsis "Python interface to the awesome mpv media player")
+    (description "Python interface to the awesome mpv media player")
+    (license license:agpl3+)))
